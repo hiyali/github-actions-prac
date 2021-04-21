6082daa078d9bb0bf7565a545c4b272fb091b1cb
Salam Hiyali <hiyali920@gmail.com>
2021-04-21 15:03:46 +0600

    🚀 Hola

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(): ReactElement {
+  return (
+    <div className="App">
+      Hello, world - MAS Portal 4.0 - 14 Apr, 12:36
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
